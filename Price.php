f211d86d71cd6e66fced05cff62b697ba1ca7afb
devill <devillmeister@gmail.com>
2013-11-13 20:22:34 +0100

    Pushed getCharge down

--- Price.php
+++ Price.php
@@ -6,14 +6,6 @@ abstract class Price
 {
     abstract public function getPriceCode();
 
-    public function getCharge($daysRented)
-    {
-        $result = 1.5;
-        if ($daysRented > 3)
-            $result += ($daysRented - 3) * 1.5;
-        return $result;
-    }
-
     public function getFrequentRenterPoints($daysRented)
     {
         if (($this->getPriceCode() == Movie::NEW_RELEASE) && $daysRented > 1)
@@ -28,6 +20,14 @@ class ChildrensPrice extends Price
     public function getPriceCode()
     {
         return Movie::CHILDRENS;
+    }
+
+    public function getCharge($daysRented)
+    {
+        $result = 1.5;
+        if ($daysRented > 3)
+            $result += ($daysRented - 3) * 1.5;
+        return $result;
     }
 }
 
